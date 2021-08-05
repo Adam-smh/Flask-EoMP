c7061c0647118646530557254ab97b188f2e4a37
Adam-smh <africa.adam1@gmail.com>
2021-08-05 16:17:45 +0200

    just need oop

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# all imports
 import hmac
 import sqlite3
 import datetime
@@ -5,13 +6,25 @@ import datetime
 from flask_cors import CORS
 from flask import Flask, request, jsonify
 from flask_jwt import JWT, jwt_required, current_identity
+from flask_mail import Mail, Message
 
 
+# User class
 class User(object):
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
         self.password = password
+
+
+# Product class
+class Product(object):
+    def __init__(self, prod_id, prod_name, prod_price, prod_desc, prod_type):
+        self.prod_id = prod_id
+        self.prod_name = prod_name
+        self.prod_price = prod_price
+        self.prod_desc = prod_desc
+        self.prod_type = prod_type
 
 
 def fetch_users():
@@ -27,6 +40,7 @@ def fetch_users():
     return new_data
 
 
+# creating user table with sql in database
 def init_user_table():
     conn = sqlite3.connect('PoS.db')
     print("Opened database successfully")
@@ -34,12 +48,14 @@ def init_user_table():
     conn.execute("CREATE TABLE IF NOT EXISTS user(user_id INTEGER PRIMARY KEY AUTOINCREMENT,"
                  "first_name TEXT NOT NULL,"
                  "last_name TEXT NOT NULL,"
+                 "email TEXT NOT NULL,"
                  "username TEXT NOT NULL,"
                  "password TEXT NOT NULL)")
     print("user table created successfully")
     conn.close()
 
 
+# creating product table with sql in database
 def init_product_table():
     conn = sqlite3.connect('PoS.db')
     print("Database opened successfully")
@@ -76,6 +92,13 @@ app = Flask(__name__)
 CORS(app)
 app.debug = True
 app.config['SECRET_KEY'] = 'super-secret'
+app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(hours=12)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'adamafrica.dev@gmail.com'
+app.config['MAIL_PASSWORD'] = 'TaxiDriver8'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 jwt = JWT(app, authenticate, identity)
 
@@ -87,19 +110,33 @@ def user_registration():
     if request.method == "POST":
         first_name = request.form['first_name']
         last_name = request.form['last_name']
+        email = request.form['email']
         username = request.form['username']
         password = request.form['password']
+
+        if not first_name or not last_name or not email or not username or not password:
+            response["message"] = "Error, please fill all fields."
+            response["status_code"] = 400
 
         with sqlite3.connect("PoS.db") as conn:
             cursor = conn.cursor()
             cursor.execute("INSERT INTO user("
                            "first_name,"
                            "last_name,"
+                           "email,"
                            "username,"
-                           "password) VALUES(?, ?, ?, ?)", (first_name, last_name, username, password))
+                           "password) VALUES(?, ?, ?, ?, ?)", (first_name, last_name, email, username, password))
             conn.commit()
             response["message"] = "success"
             response["status_code"] = 201
+
+            mail = Mail(app)
+            msg = Message("Welcome!", sender='adamafrica.dev@gmail.com', recipients=[email])
+            msg.body = "Hi there {}!\n".format(username)
+            msg.body = msg.body + "Your profile has been registered successfully\n"
+            msg.body = msg.body + "Please feel free to send us email if you have any queries or concerns.\n\n " \
+                                  "Kind regards,\n Shopify Team"
+            mail.send(msg)
         return response
 
 
@@ -133,40 +170,133 @@ def login():
 @jwt_required()
 def add_product():
     response = {}
+    try:
+        if request.method == "POST":
+            product_name = request.form["product_name"]
+            product_price = request.form["product_price"]
+            product_description = request.form["product_description"]
+            product_type = request.form["product_type"]
 
-    if request.method == "POST":
-        p_id = request.form["p_id"]
-        p_name = request.form["p_name"]
-        p_price = request.form["p_price"]
-        p_description = request.form["p_description"]
-        p_type = request.form["p_type"]
+            if not product_name or not product_price or not product_description or not product_type:
+                response["message"] = "Error, please fill all fields."
+                response["status_code"] = 400
 
-        with sqlite3.connect('PoS.db') as conn:
-            cursor = conn.cursor()
-            cursor.execute("INSERT INTO product("
-                           "product_id,"
-                           "product_name,"
-                           "product_price,"
-                           "product_description,"
-                           "product_type) VALUES(?, ?, ?)", (p_id, p_name, p_price, p_description, p_type))
-            conn.commit()
-            response["status_code"] = 201
-            response['description'] = "Product added successfully"
+            int(product_price)
+
+            with sqlite3.connect('PoS.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO product("
+                               "product_name,"
+                               "product_price,"
+                               "product_description,"
+                               "product_type) VALUES(?, ?, ?, ?)",
+                               (product_name, product_price, product_description, product_type))
+                conn.commit()
+                response["status_code"] = 201
+                response['description'] = "Product added successfully"
+            return response
+    except ValueError:
+        response['message'] = "Error, please make sure price field is a number."
+        response['status_code'] = 401
         return response
 
 
-# @app.route('/get-product/', methods=["GET"])
-# def get_product():
-#     response = {}
-#     with sqlite3.connect("PoS.db") as conn:
-#         cursor = conn.cursor()
-#         cursor.execute("SELECT * FROM post")
-#
-#         posts = cursor.fetchall()
-#
-#     response['status_code'] = 200
-#     response['data'] = posts
-#     return response
+@app.route('/show-products/', methods=["GET"])
+def get_product():
+    response = {}
+    with sqlite3.connect("PoS.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM product")
+
+        products = cursor.fetchall()
+
+    response['status_code'] = 200
+    response['data'] = products
+    return response
+
+
+@app.route('/view-product/<int:product_id>/')
+def view_product(product_id):
+    response = {}
+
+    with sqlite3.connect('PoS.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM product WHERE product_id={}'.format(product_id))
+        product = cursor.fetchone()
+
+    response['message'] = 'Product info fetched.'
+    response['status_code'] = 200
+    response['product'] = product
+
+    return response
+
+
+@app.route('/edit-product/<int:product_id>/', methods=['PUT'])
+@jwt_required()
+def edit_product(product_id):
+    response = {}
+
+    if request.method == 'PUT':
+        incoming_data = dict(request.json)
+        put_data = {}
+
+        if incoming_data.get('product_name'):
+            put_data['product_name'] = incoming_data.get('product_name')
+            with sqlite3.connect('PoS.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE product SET product_name=? WHERE product_id=?", (put_data["product_name"],
+                                                                                        product_id))
+                conn.commit()
+                response['message'] = "Changes successfully made."
+                response['status_code'] = 200
+
+        if incoming_data.get('product_price'):
+            put_data['product_price'] = incoming_data.get('product_price')
+            with sqlite3.connect('PoS.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE product SET product_price=? WHERE product_id=?", (put_data["product_price"],
+                                                                                         product_id))
+                conn.commit()
+                response['message'] = "Changes successfully made."
+                response['status_code'] = 200
+
+        if incoming_data.get('product_description'):
+            put_data['product_description'] = incoming_data.get('product_description')
+            with sqlite3.connect('pointOfSale.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE product SET product_description=? WHERE product_id=?",
+                               (put_data["product_description"], product_id))
+                conn.commit()
+                response['message'] = "Changes successfully made."
+                response['status_code'] = 200
+
+        if incoming_data.get('product_type'):
+            put_data['product_type'] = incoming_data.get('product_type')
+            with sqlite3.connect('PoS.db') as conn:
+                cursor = conn.cursor()
+                cursor.execute("UPDATE product SET product_type=? WHERE product_id=?",
+                               (put_data["product_type"], product_id))
+                conn.commit()
+                response['message'] = "Changes successfully made."
+                response['status_code'] = 200
+
+    return response
+
+
+@app.route('/delete-product/<int:product_id>/')
+@jwt_required()
+def delete_product(product_id):
+    response = {}
+
+    with sqlite3.connect('PoS.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM product WHERE product_id={}'.format(product_id))
+        conn.commit()
+
+        response['message'] = 'Product successfully removed.'
+        response['status_code'] = 200
+
+    return response
 
 
 if __name__ == '__main__':
